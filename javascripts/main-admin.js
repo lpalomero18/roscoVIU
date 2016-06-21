71f8b77ae9f230396946131845389be448f2fc6e
Luis Palomero <LPALOMERO@informatica.local>
2016-06-21 13:45:39 +0200

    version incompleta que ya usa AWS para la sinconizacion de la puntuacion del primer equipo

--- javascripts/main-admin.js
+++ javascripts/main-admin.js
@@ -1,5 +1,5 @@
 // Esto para establecer los contadores al principio
-	var baseURL="https://cache-aws-us-east-1.iron.io/1/projects/5767de59638de900078dc881/caches/puntuaciones"
+/*	var baseURL="https://cache-aws-us-east-1.iron.io/1/projects/5767de59638de900078dc881/caches/puntuaciones"
 	var urlpuntos1 = baseURL+"/items/punt1?oauth=JdhnN1iMVLth2NlH4m7h";
 	var urlpuntos2 =baseURL+"/items/punt2?oauth=JdhnN1iMVLth2NlH4m7h";
 	var credenciales="JdhnN1iMVLth2NlH4m7h";
@@ -23,7 +23,7 @@ function coge_ajax(url_name, container){
     
 	});
 
-
+*/
 
 // Todo esto para mover el contador
 	var up1		= $('.equipo1 .glyphicon-triangle-top').asEventStream('click');
@@ -32,31 +32,35 @@ function coge_ajax(url_name, container){
 	var down2	= $('.equipo2 .glyphicon-triangle-bottom').asEventStream('click');
 
 	var counter1 =
-		up1.map(1).scan(0, function(x,y){return x+y});
+		up1.map(1).merge(down1.map(-1)).scan(0, function(x,y){return x+y});
 	var counter2 =
 		up2.map(1).merge(down2.map(-1)).scan(0, function(x,y){return x+y});
+	// See the Configuring section to configure credentials in the SDK
+	AWS.config.update({accessKeyId: 'AKIAIPYYGPZCQTRWPI4Q', secretAccessKey: 'OasGV9UxvXHQmABvDtBzezwp5/THW6nkv5eXulbE'});
+
+  	// Configure your region
+  	AWS.config.region = 'eu-west-1';
+
+
+  	var bucket = new AWS.S3({params: {Bucket: 'pruebas-luis'}});
 	counter1.assign($('.equipo1 .puntuacion-puntos'), 'text');
 	counter2.assign($('.equipo2 .puntuacion-puntos'), 'text');
-/*	incrementaCounter = counter1.changes().map($.post( 
-		baseURL+"/items/punt1/clear?oauth=JdhnN1iMVLth2NlH4m7h",
-		{
-			"amount": 1
-		},
-	    function(data, status){
-    	    alert("Data: " + data + "\nStatus: " + status);
-	    }
+	var contenido;
+	function actualiza(){
+		var temporal =$('.equipo1 .puntuacion-puntos').text();
+		if (contenido != temporal ) {
+			contenido = temporal;		
+			var params =			{Key: 'equipo1.txt', ACL: 'public-read', Body: contenido}
+			console.log(params);
+			bucket.upload( params, function(err, data){
+			console.log(err + " " + data);
+		});
 
-	));
-*/
-		incrementaCounter = counter1.changes().map($.ajax({ 
-		
-			type: 'PUT', 
-			url: baseURL+"/items/punt1?oauth=JdhnN1iMVLth2NlH4m7h",
-//			url: baseURL+"/items/punt1/increment?oauth=JdhnN1iMVLth2NlH4m7h",
-			data:{
-				"value": $('.equipo1 .puntuacion-puntos').text(),  
-				"expires_in": 86400,
-  				"replace": true
-			}
-		
-	}));
+		}
+		setTimeout(actualiza, 1000);
+	}	
+	actualiza();
+
+ 
+
+ 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,5 @@
+
+
 function coge_ajax(url_name, container){
 	var respuesta;
 	$.ajax({ 
@@ -12,10 +14,13 @@ function coge_ajax(url_name, container){
 }
 
 function observa(){
-	var urlpuntos1 ="https://cache-aws-us-east-1.iron.io/1/projects/5767de59638de900078dc881/caches/puntuaciones/items/punt1?oauth=JdhnN1iMVLth2NlH4m7h";
+/*	var urlpuntos1 ="https://cache-aws-us-east-1.iron.io/1/projects/5767de59638de900078dc881/caches/puntuaciones/items/punt1?oauth=JdhnN1iMVLth2NlH4m7h";
 	var urlpuntos2 ="https://cache-aws-us-east-1.iron.io/1/projects/5767de59638de900078dc881/caches/puntuaciones/items/punt2?oauth=JdhnN1iMVLth2NlH4m7h";
 	punt1 =  coge_ajax(urlpuntos1, ".equipo1 .puntuacion-puntos");
 	punt2 = coge_ajax(urlpuntos2, ".equipo2 .puntuacion-puntos");
-	setTimeout(observa, 10000);
+*/	
+	$(".equipo1 .puntuacion-puntos").load('https://s3-eu-west-1.amazonaws.com/pruebas-luis/equipo1.txt');
+	$(".equipo2 .puntuacion-puntos").load('https://s3-eu-west-1.amazonaws.com/pruebas-luis/equipo2.txt');
+	setTimeout(observa, 2000);
 }
 observa();
